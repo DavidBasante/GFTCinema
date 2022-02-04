3355884a3e4625213d62c4802fc746367b9e5cd4
david <david@DESKTOP-P99GIPF>
2022-02-04 18:12:01 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Cinema.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFTCinema.Entities
+{
+    internal class Cinema
+    {
+        public Cinema(string Nome, string Endereco, List<Filme> filmeEmCartaz)
+        {
+            this.Nome = Nome;
+            this.Endereco = Endereco;
+            this.filmeEmCartaz = filmeEmCartaz;
+        }
+
+        public Cinema()
+        {
+        }
+
+        public string Nome;
+
+        public string Endereco;
+
+        public List<Filme> filmeEmCartaz;
+
+    }
+}
---
+++ Entities/Filme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFTCinema.Entities
+{
+    internal class Filme
+    {
+        public Filme(string titulo, string Genero, int DuracaoEmMinutos, string horarioSessao)
+        {
+            this.titulo = titulo;
+            this.Genero = Genero;
+            this.DuracaoEmMinutos = DuracaoEmMinutos;
+            this.horarioSessao = horarioSessao;
+        }
+        public Filme()
+        {
+
+        }
+
+
+        public string titulo;
+
+        public string Genero;
+
+        public int DuracaoEmMinutos;
+
+        public string horarioSessao;
+    }
+}
